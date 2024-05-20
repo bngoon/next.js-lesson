07abd6db2b852611a236b1b5825b74d411ca0e5b
Booker <bookerngoon@gmail.com>
2024-05-20 13:39:43 -0400

    static and dynamic rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,12 +6,13 @@ interface User {
   email: string;
 }
 const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users', {next: {revalidate: 10 }});
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {cache: 'no-store'});
   const users: User[] = await res.json();
 
   return (
     <>
     <h1><strong>Users</strong></h1>
+    <p>{new Date().toLocaleTimeString()}</p>
     <ul>
       {users.map(user => <li key={user.id}>{user.name} | {user.email} </li>)}
     </ul>
