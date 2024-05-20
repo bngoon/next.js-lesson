4f083e9916a150cafd175f8ce47c72aaea1ed16e
Booker <bookerngoon@gmail.com>
2024-05-20 13:32:59 -0400

    data caching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,14 +6,14 @@ interface User {
   email: string;
 }
 const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {next: {revalidate: 10 }});
   const users: User[] = await res.json();
 
   return (
     <>
-    <h1>Users</h1>
+    <h1><strong>Users</strong></h1>
     <ul>
-      {users.map(user => <li key={user.id}>{user.name} </li>)}
+      {users.map(user => <li key={user.id}>{user.name} | {user.email} </li>)}
     </ul>
     </>
   )
